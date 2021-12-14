10e3a0674e1fb79d5147ffea30023c3f883c9ded
skeanster <ryanskeans@gmail.com>
2021-12-14 13:22:21 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import imgs from './imagesExport';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    PlanetList: null,
+  });
+
+  useEffect(() => {
+    fetch('https://swapi-graphql.netlify.app/.netlify/functions/index', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        query: `
+      query ExampleQuery {
+        allPlanets {
+          planets {
+            id
+            name
+            population
+            climates
+            surfaceWater
+            terrains
+            gravity
+            filmConnection {
+              films {
+                title
+              }
+            }
+          }
+        }
+      }
+      `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const trueData = data.data.allPlanets.planets.filter(
+          (planet) => planet.filmConnection.films.length >= 1
+        );
+        setState({ PlanetList: trueData });
+      });
+  }, []);
+
+  if (state.PlanetList === null) {
+    return (
+      <div>
+        <div>data not loaded</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        {state.PlanetList.map((planet) => {
+          return (
+            <img
+              src={imgs[planet.name]}
+              key={planet.id}
+              id={planet.id}
+              alt={planet.name + 'picture'}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/imagesExport.js
@@ -0,0 +1,45 @@
+import Tatooine from './planetPictures/Tatooine.gif';
+import Alderaan from './planetPictures/Alderaan.gif';
+import YavinIV from './planetPictures/YavinIV.gif';
+import Hoth from './planetPictures/Hoth.gif';
+import Dagobah from './planetPictures/Dagobah.gif';
+import Bespin from './planetPictures/Bespin.gif';
+import Endor from './planetPictures/Endor.gif';
+import Naboo from './planetPictures/Naboo.gif';
+import Coruscant from './planetPictures/Coruscant.gif';
+import Kamino from './planetPictures/Kamino.gif';
+import Geonosis from './planetPictures/Geonosis.gif';
+import Utapau from './planetPictures/Utapau.gif';
+import Mustafar from './planetPictures/Mustafar.gif';
+import Kashyyyk from './planetPictures/Kashyyyk.gif';
+import PolisMassa from './planetPictures/PolisMassa.gif';
+import Mygeeto from './planetPictures/Mygeeto.gif';
+import Felucia from './planetPictures/Felucia.gif';
+import CatoNeimoidia from './planetPictures/CatoNeimoidia.gif';
+import Saleucami from './planetPictures/Saleucami.gif';
+import OrdMantell from './planetPictures/OrdMantell.gif';
+
+const imgs = {
+  Tatooine: Tatooine,
+  Alderaan: Alderaan,
+  'Yavin IV': YavinIV,
+  Hoth: Hoth,
+  Dagobah: Dagobah,
+  Bespin: Bespin,
+  Endor: Endor,
+  Naboo: Naboo,
+  Coruscant: Coruscant,
+  Kamino: Kamino,
+  Geonosis: Geonosis,
+  Utapau: Utapau,
+  Mustafar: Mustafar,
+  Kashyyyk: Kashyyyk,
+  'Polis Massa': PolisMassa,
+  Mygeeto: Mygeeto,
+  Felucia: Felucia,
+  'Cato Neimoidia': CatoNeimoidia,
+  Saleucami: Saleucami,
+  'Ord Mantell': OrdMantell,
+};
+
+export default imgs;
